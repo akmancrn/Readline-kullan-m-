93baa6f41f925fb9420e9deeb60d78d00314c256
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 12:30:32 +0300

    Create Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import java.io.PrintStream
+
+fun main (args : Array<String>)
+{
+
+
+   println("Sisteme giriş yapmak için lütfen sizden sizden istenen bilgileri doğru giriniz")
+   println("Adınızı ve Soyadınızı giriniz")
+   var isim = readLine()!!
+
+   println("yaşınızı giriniz")
+   var yas = readLine()!!
+
+}
